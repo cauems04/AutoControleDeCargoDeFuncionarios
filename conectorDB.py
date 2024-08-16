830128121d08ed1eaea2412fc38e0888e1491b78
cauems04 <136491175+cauems04@users.noreply.github.com>
2024-08-16 15:39:40 -0300

    Update conectorDB.py

--- conectorDB.py
+++ conectorDB.py
@@ -4,10 +4,10 @@ def conectar_banco_de_dados(banco_nome):
     try:
         # Estabeleça a conexão com o banco de dados
         conexao = mysql.connector.connect(
-            host = "10.59.208.183",
-            port = "3306",
-            user = "root",
-            password = "0730@info",
+            host = "ip do host",
+            port = "porta de acesso",
+            user = "usuario",
+            password = "senha",
             database = banco_nome
         )
 
